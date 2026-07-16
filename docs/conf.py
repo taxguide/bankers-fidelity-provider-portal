fdc162226ec14e2427e293bd85a06c736edc5567
taxguide <singhparash06@gmail.com>
2026-07-16 16:21:38 +0530

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,1 +1,11 @@
+project = 'Bankers Fidelity Provider Portal'
+author = 'Documentation Guide'
+release = '1.0'
 
+extensions = []
+
+templates_path = []
+
+exclude_patterns = []
+
+html_theme = 'alabaster'
